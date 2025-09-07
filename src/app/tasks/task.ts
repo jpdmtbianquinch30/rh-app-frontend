10061ba56db638763524547663d973f80806fb92
Jean Philippe Bianquinch <jeanphilippebianquinch@gmail.com>
2025-09-07 21:54:53 +0000

    Deleted shared-module.ts

--- src/app/tasks/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id: number;
-  title: string;
-  description: string;
-  assignedTo: number; // employee id
-  status: 'PENDING' | 'IN_PROGRESS' | 'COMPLETED';
-  dueDate: string;
-}
